e59a41292c8da85ca4164de1d5d5e1f525b40c6f
ijo0r98 <ijo0r98@naver.com>
2021-08-02 10:24:44 +0900

    edit settings urls

--- cv_project/settings.py
+++ cv_project/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'opencv_webapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'opencv_webapp',
 ]
 
 MIDDLEWARE = [
